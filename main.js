ecbf1866355bc14ff86c683a61c002533a76b9cc
Phrodus <70674167+Phrodus@users.noreply.github.com>
2020-09-12 21:37:25 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let subheading = document.querySelector('h3');
+let image = document.querySelector('img');
+
+function feedback() {
+	alert('Please e-mail Phrodus at username@website.com to give feedback.')
+	prompt('This is a test box. Your input will not be recorded.')   
+}
